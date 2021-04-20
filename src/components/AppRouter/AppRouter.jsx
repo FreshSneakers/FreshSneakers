c94aaca55a8907b3980c21c10ca99de93e3c9a2b
Eduardo García <edugarciasb16@gmail.com>
2021-04-20 21:10:17 +0200

    base front-end

---
+++ src/components/AppRouter/AppRouter.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Switch } from 'react-router';
+import Home from '../Home/Home';
+import Login from '../Auth/Login'
+import SignUp from'../Auth/SignUp'
+
+const AppRouter = () => {
+    return (
+       <Switch>
+           <Route exact path="/" component={Home}/>
+           <Route exact path="/login" component={Login}/>
+           <Route exact path="/signup" component={SignUp}/>
+       </Switch>
+    );
+};
+
+export default AppRouter;
