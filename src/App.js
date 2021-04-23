e9fd36984d906f259862c91d6b69f6748fbd38ff
rgarsan <rgarsan84@gmail.com>
2021-04-23 23:51:33 +0200

    login done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import { useState } from 'react';
-import './App.css';
-import AppRouter from './components/AppRouter/AppRouter';
-import NavBar from './components/NavBar/NavBar'
+import { useEffect, useState } from "react";
+import "./App.css";
+//import AppRouter from './components/AppRouter/AppRouter';
+import NavBar from "./components/NavBar/NavBar";
+import { Route, Switch } from "react-router";
+import { getUserInfo } from "./services/UserService";
+import Home from "./components/Home/Home";
+import Login from "./components/Auth/Login";
+import SignUp from "./components/Auth/SignUp";
+import { getAccessToken } from "./stores/AccessTokenStore";
 
 function App() {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
+
+  const getUser = () => {
+    return getUserInfo().then((response) => setUser(response));
+  };
+
+  useEffect(()=>{
+    if(getAccessToken){
+      getUser()
+    }
+  },[])
 
   return (
     <div className="App">
-      <NavBar />
-      <AppRouter />
+      <NavBar user= {user}/>
+      {/*<AppRouter />*/}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" render={() => <Login doLogin={getUser} />} />
+        <Route exact path="/signup" component={SignUp} />
+      </Switch>
     </div>
   );
 }
--- src/services/BaseService.js
+++ src/services/BaseService.js
@@ -1,26 +1,34 @@
 import axios from 'axios'
-import {getAccessToken} from '../stores/AccessTokenStore'
+import {getAccessToken,logout} from '../stores/AccessTokenStore'
+
 
 
 export const create = (opts = {}) => {
     const http = axios.create({
-        baseURL:'http://localhost:3001/api',
-        ...opts
+      baseURL: 'http://localhost:3001/api',
+      ...opts
     })
-
+  
     http.interceptors.request.use(request => {
-        if(opts.userAccessToken !== false){
-            request.headers.common.Authorization = `Bearer ${getAccessToken()}`
-        }else{
-            delete request.headers.common.Authorization
-        }
-
-        return request
+      if (opts.useAccessToken !== false) {
+        request.headers.common.Authorization = `Bearer ${getAccessToken()}`
+      } else {
+        delete request.headers.common.Authorization
+      }
+  
+      return request
     })
-
+  
     http.interceptors.response.use(
-        response => response.data
+      response => response.data,
+      error => {
+        if (error.response && [401, 403].includes(error.response.status)) {
+          logout()
+        }
+  
+        return Promise.reject(error)
+      }
     )
-
+  
     return http
-}
+  }
